e411e76c66c886bf36aa0082719221caea8d0825
Luc Libralesso <libralesso.l@gmail.com>
2015-11-23 20:40:34 -0500

    add projet

---
+++ projet/execution/alpine/execution.bat
@@ -0,0 +1,6 @@
+REM L'executable (LaboPSO.exe) doit etre dans le meme repertoire
+
+REM Liste des paramètres
+REM 1)EXECUTABLE 2)TAILLE Essaim 	3)C1 	4)C2 	5)C3 	6)NB_EVAL_MAX
+
+LaboPSO.exe	 100	 0.8 1.62 1.62 10000
